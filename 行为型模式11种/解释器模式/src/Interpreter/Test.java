823664179ac835d2a684c3979249117d0ed5448c
westlife <289536143@qq.com>
2017-12-28 14:42:57 +0800

    23种设计模式

--- 行为型模式11种/解释器模式/src/Interpreter/Test.java
+++ 行为型模式11种/解释器模式/src/Interpreter/Test.java
@@ -4,7 +4,7 @@ package Interpreter;
  * @Author: westlife
  * @Description:
  * @Date: Created in 14:27 2017/12/28
- * 基本就这样，解释器模式用来做各种各样的解释器，如正则表达式等的解释器等等！
+ * 基本就这样，解释器模式用来做各种各样的解释器，如正则表达式等的解释器等等！、
  */
 public class Test {
     public static void main(String[] args) {
